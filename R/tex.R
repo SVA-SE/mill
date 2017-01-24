b09eebe5bc341df7589d89d06a5a3a26c11f87bc
Stefan Widgren <stefan.widgren@gmail.com>
2017-01-24 16:34:50 +0100

    Add internal method to clean tex file

---
+++ R/tex.R
@@ -0,0 +1,45 @@
+##' @keywords internal
+clean_tex <- function(tex) {
+    ## Remove \begin{multicols}
+    pattern <- "[\\]begin[{]multicols[}][{]2[}]"
+    tex <- gsub(pattern, "", tex)
+
+    ## Remove \end{multicols}
+    pattern <- "[\\]end[{]multicols[}]"
+    tex <- gsub(pattern, "", tex)
+
+    ## Remove use of \numprint{1234}
+    pattern <- "[\\]numprint[{]([0-9]*)[}]"
+    tex <- gsub(pattern, "\\1", tex)
+
+    ## Remove use of \label{sec:some-section}
+    pattern <- "[\\]label[{]sec:[^}]*[}]"
+    tex <- gsub(pattern, "", tex)
+
+    ## Simplify
+    ## e.g. \ref{fig:some-chapter:4}
+    ## to \ref{fig:4}
+    pattern <- "[\\]ref[{]fig:[^:]*:([0-9]*)[}]"
+    tex <- gsub(pattern, "\\\\ref{fig:\\1}", tex)
+
+    ## Simplify
+    ## e.g. \ref{tab:some-chapter:4}
+    ## to \ref{tab:4}
+    pattern <- "[\\]ref[{]tab:[^:]*:([0-9]*)[}]"
+    tex <- gsub(pattern, "\\\\ref{tab:\\1}", tex)
+
+    ## Remove '\\\\'
+    pattern <- "^[\\]{4}$"
+    tex <- gsub(pattern, "", tex)
+
+    ## Remove '\noindent'
+    pattern <- "^[\\]noindent$"
+    tex <- gsub(pattern, "", tex)
+
+    ## Remove picture
+    ## e.g. "\\includepicture{image1.png}{Photo: Alice}{0cm 0cm 0cm 0cm}"
+    pattern <- "^[\\]includepicture[{][^}]*[}][{][^}]*[}][{][^}]*[}]$"
+    tex <- gsub(pattern, "", tex)
+
+    tex
+}
