1af3f5d9ffab3fc99003bc0efb545f4baf0e0ffe
Stefan Widgren <stefan.widgren@gmail.com>
2017-01-25 21:48:44 +0100

    Add org mode file items

--- R/init.R
+++ R/init.R
@@ -176,11 +176,37 @@ to_orgmode.report <- function(x) {
 }
 
 to_orgmode.chapters <- function(x) {
-    c("* [[file+emacs:chapters][Chapters]] [%]", sapply(x, function(y) to_orgmode(y)))
+    c("* [[file+emacs:chapters][Chapters]] [%]",
+      unlist(lapply(x, function(y) to_orgmode(y))))
 }
 
 to_orgmode.chapter <- function(x) {
-    paste0("** TODO [[file+emacs:chapters/",
-           gsub("[[:space:]]", "%20", x$title),
-           "][", x$title, "]]")
+    lines <- paste0("** TODO [[file+emacs:chapters/",
+                    gsub("[[:space:]]", "%20", x$title),
+                    "][", x$title, "]]")
+
+    lines <- c(lines,
+               orgmode_file_items(x$path, x$title, "table"),
+               orgmode_file_items(x$path, x$title, "figure"))
+
+    lines
+}
+
+##' @keywords internal
+orgmode_file_items <- function(path, title, file_type) {
+    items <- character()
+
+    files <- list.files(path = path,
+                        pattern = paste0("^", file_type, "-[0-9]+[.]tex$"))
+
+    if (length(files)) {
+        items <- sapply(files, function(filename) {
+            paste0("*** TODO [[file:chapters/",
+                   gsub("[[:space:]]", "%20", title),
+                   "/", filename,
+                   "][", sub("[.]tex$", "", filename), "]]")
+        }, USE.NAMES = FALSE)
+    }
+
+    items
 }
