3d3f0ab2be3daf37ceb913cc438daa44e9b6b28f
Stefan Widgren <stefan.widgren@gmail.com>
2017-01-25 13:18:32 +0100

    Change link to each chapter in the org-mode file

--- R/init.R
+++ R/init.R
@@ -178,9 +178,7 @@ to_orgmode.chapters <- function(x) {
 }
 
 to_orgmode.chapter <- function(x) {
-    paste0("** TODO [[file:chapters/",
+    paste0("** TODO [[file+emacs:chapters/",
            gsub("[[:space:]]", "%20", x$title),
-           "/text.tex][",
-           x$title,
-          "]]")
+           "][", x$title, "]]")
 }
