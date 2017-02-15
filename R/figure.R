51ae21ea556e2e77f26296c4af3b6a87d10ca460
Thomas Rosendal <thomas.rosendal@sva.se>
2017-02-15 17:20:55 +0100

    preview figures method.
    Needs some attention because it doesn't work for chapters with spaces in
    the title :) I'll have a look at this later

--- R/figure.R
+++ R/figure.R
@@ -43,3 +43,42 @@ build_figures.chapter <- function(x, png = FALSE) {
 do_build_figure <- function(figure, png) {
     source(figure, local = TRUE, chdir = TRUE)
 }
+
+
+###########################################################33
+
+##' @export
+preview_figures <- function(x) UseMethod("preview_figures")
+
+##' @export
+preview_figures.report <- function(x) {
+    preview_figures(x$chapters)
+}
+
+##' @export
+preview_figures.chapters <- function(x) {
+    lapply(x, function(y) preview_figures(y))
+    invisible()
+}
+
+##' @export
+preview_figures.chapter <- function(x) {
+    lapply(figure_files(x, "tex"), do_preview_figure)
+    invisible()
+}
+
+##' @keywords internal
+do_preview_figure <- function(figure) {
+    fig_dir <- dirname(figure)
+    fig_name <- paste0("preview-", tools::file_path_sans_ext(basename(figure)), ".pdf")
+    filename <- tempfile(tmpdir = fig_dir, fileext = ".tex")
+    assets <- file.path(dirname(dirname(fig_dir)), "assets")
+    tex <- c(readLines(file.path(assets, "figure-preview/snippet1.tex")),
+             readLines(figure),
+             readLines(file.path(assets, "figure-preview/snippet2.tex")))
+    writeLines(tex, filename)
+    system(paste("cd ", fig_dir, ";lualatex ", basename(filename)))
+    file.copy(paste0(tools::file_path_sans_ext(filename), ".pdf"),
+              file.path(fig_dir, fig_name), overwrite = TRUE)
+    unlink(paste0(tools::file_path_sans_ext(filename), "*"))
+}
