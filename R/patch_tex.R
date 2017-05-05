ed3032dab68a90f9c65ad7514ba101f8c8bd7748
Stefan Widgren <stefan.widgren@gmail.com>
2017-05-05 18:10:22 +0200

    Refactoring of 'apply_patch.chapter'

--- R/patch_tex.R
+++ R/patch_tex.R
@@ -139,7 +139,7 @@ apply_patch <- function(x) {
 ##' Apply the patch to the chapter
 ##'
 ##' @param x A chapter
-##' @return the path to typeset.tex
+##' @return invisible(NULL)
 ##' @export
 apply_patch.chapter <- function(x) {
     owd <- setwd(x$path)
@@ -149,10 +149,13 @@ apply_patch.chapter <- function(x) {
                                         "-o", "typeset.tex"),
                                stdout = TRUE, stderr = TRUE),
                        warning = function(w) w)
-    if (identical(output,
-                  "patching file typeset.tex (read from text.tex)"))
-        return(typeset)
-    stop(paste("Unable to apply patch: ", file.path(x$path, "typeset.patch")))
+    if (!identical(output,
+                   "patching file typeset.tex (read from text.tex)")) {
+        cat(output, "\n")
+        stop(paste("Unable to apply patch: ", x$title))
+    }
+
+    invisible(NULL)
 }
 
 ##' Build_patch
