33de87d973aac454177662b300a18361b1375d5d
Stefan Widgren <stefan.widgren@gmail.com>
2017-05-18 18:34:20 +0200

    Refactoring of 'reduce_image'

--- R/images.R
+++ R/images.R
@@ -1,22 +1,24 @@
 ##' Reduce Images
 ##'
-##' @param path The original
-##' @return A path to the reduced image
+##' @param from filename of the original image
+##' @param to filename of the reduced image
+##' @return invisible NULL
 ##' @export
-reduce_image <- function(path) {
-    ext <- tools::file_ext(path)
-    stopifnot(ext %in% c("jpg", "png"))
-    reduced <- tempfile(fileext = paste0(".", ext))
-    path <- normalizePath(path)
+reduce_image <- function(from, to) {
+    from <- normalizePath(from, mustWork = TRUE)
 
-    args <- switch(ext,
+    args <- switch(tools::file_ext(from),
                    jpg = c("-resize 1000x", "-compress JPEG", "-quality 50"),
-                   png = "-resize 75%")
+                   png = "-resize 75%",
+                   NULL)
+
+    if (is.null(args))
+        stop("Unsupported image format")
 
     system2("convert",
-            args = c(shQuote(path), args, reduced),
+            args = c(shQuote(from), args, to),
             stdout = TRUE,
             stderr = TRUE)
 
-    reduced
+    invisible()
 }
