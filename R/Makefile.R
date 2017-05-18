48331d345835dbc27f75b4d90bf7ef3b5ae4453e
Stefan Widgren <stefan.widgren@gmail.com>
2017-05-18 11:39:02 +0200

    Add 'create_Makefile' function to add a Makefile in each chapter

---
+++ R/Makefile.R
@@ -0,0 +1,38 @@
+##' Create a Makefile in each chapter of the report
+##'
+##' @param x The report object
+##' @param repo The git repository to add the 'Makefile' to.
+##' @param ... Additional arguments.
+##' @return invisible NULL
+##' @export
+create_Makefile <- function(x, ...) UseMethod("create_Makefile")
+
+##' @export
+create_Makefile.report <- function(x, ...) {
+    repo <- git2r::repository(x$path)
+    lapply(x$chapters, function(y) create_Makefile(y, repo = repo))
+    invisible()
+}
+
+##' @export
+create_Makefile.chapter <- function(x, repo, ...) {
+    lines <- c("diff:",
+               "\tdiff -c --label=text --label=typeset text.tex typeset.tex > typeset.patch; [ $$? -eq 1 ]",
+               "",
+               "patch:",
+               "\tpatch text.tex -i typeset.patch -o typeset.tex",
+               "",
+               "roundtrip:",
+               sprintf("\tcd ../.. && Rscript -e \"library('relax'); r <- load_report(); roundtrip(r[['%s']])\"",
+                       x$title, x$title),
+               "",
+               "PHONY: diff, patch, roundtrip",
+               "")
+
+    writeLines(lines, file.path(x$path, "Makefile"))
+
+    if (!is.null(repo))
+        git2r::add(repo, file.path(x$path, "Makefile"))
+
+    invisible()
+}
