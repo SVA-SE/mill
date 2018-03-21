a67d945f5992b10cfd696ee24f29d02e5746a9c6
Thomas Rosendal <thomas.rosendal@sva.se>
2018-03-21 13:57:16 +0100

    Added draft of function to call compare::imagemagick
    This gives you the diff of two images

--- R/images.R
+++ R/images.R
@@ -1,5 +1,7 @@
 ##' Reduce Images
 ##'
+##' Reduce the resolution of images. Requires imagemagick.
+##'
 ##' @param from filename of the original image
 ##' @param to filename of the reduced image
 ##' @importFrom tools file_ext
@@ -32,4 +34,31 @@ reduce_image <- function(from, to) {
             stderr = TRUE)
 
     invisible()
+}
+
+##' Compare Images
+##'
+##' Submit an a reference image and a new image and get back the
+##' percent difference in all colour channels. Optionally you can also
+##' save the output comparison image that highlights the differences
+##' between them.
+##'
+##' Requires imagemagick
+##'
+##' @title image_diff
+##' @param reference Path to a reference image
+##' @param new Path to an image to compare to the reference image
+##' @param output Optional path to the output image
+##' @return numeric The percent difference between the two images
+##' @export
+image_diff <- function(reference, new, output = tempfile()) {
+    reference <- normalizePath(reference, mustWork = TRUE)
+    new <- normalizePath(new, mustWork = TRUE)
+    args <- c("-verbose", "-metric MAE")
+    a <- system2("compare",
+                 args = c(args, shQuote(new), shQuote(reference), shQuote(output)),
+                 stdout = TRUE,
+                 stderr = TRUE)
+    index <- grep("all: ", a)
+    as.numeric(gsub("\\(", "", regmatches(a[index], regexpr("\\([^\\)]*", a[index]))))
 }
