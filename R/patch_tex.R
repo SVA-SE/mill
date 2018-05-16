86dc124b1c631d57e824d0053de904ae90302924
Stefan Widgren <stefan.widgren@gmail.com>
2018-05-16 07:30:44 +0200

    Refactoring of 'apply_patch' function

--- R/patch_tex.R
+++ R/patch_tex.R
@@ -1,37 +1,25 @@
-##' Apply_patch
+##' Apply patch
 ##'
-##' @export
-##' @param x A chapter
-apply_patch <- function(x) {
-    UseMethod("apply_patch")
-}
-
-##' Apply_patch.report
-##'
-##' @export
-##' @param x A report object
-apply_patch.report <- function(x) {
-    lapply(chapters(x)$section, function(y) apply_patch(y))
-    invisible()
-}
-
-##' Apply_patch
-##'
-##' Apply the patch to the chapter
-##'
-##' @param x A chapter
 ##' @return invisible(NULL)
 ##' @export
-apply_patch.chapter <- function(x) {
-    output <- tryCatch(system2("patch",
-                               args = c("text.tex", "-i", "typeset.patch",
-                                        "-o", "typeset.tex"),
-                               stdout = TRUE, stderr = TRUE),
-                       warning = function(w) w)
+apply_patch <- function() {
+    if (in_chapter()) {
+        output <- tryCatch(system2("patch",
+                                   args = c("text.tex", "-i", "typeset.patch",
+                                            "-o", "typeset.tex"),
+                                   stdout = TRUE, stderr = TRUE),
+                           warning = function(w) w)
 
-    if (!identical(output,
-                   "patching file typeset.tex (read from text.tex)")) {
-        stop(paste("Unable to apply patch: ", chapter_title(x)))
+        if (!identical(output,
+                       "patching file typeset.tex (read from text.tex)")) {
+            stop(paste("Unable to apply patch: ", chapter_title(x)))
+        }
+    } else if (in_report()) {
+        lapply(list.files("chapters"), function(chapter) {
+            wd <- setwd(paste0("chapters/", chapter))
+            apply_patch()
+            setwd(wd)
+        })
     }
 
     invisible(NULL)
