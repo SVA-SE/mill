1b156791d5d021bd8b09813a439cbbd5690d8bc5
Stefan Widgren <stefan.widgren@gmail.com>
2018-05-16 07:14:01 +0200

    Refactoring of 'references' function

--- R/latex.R
+++ R/latex.R
@@ -39,30 +39,24 @@ luatex <- function(texname, clean = FALSE) {
 
 ##' TeX references
 ##'
-##' @param x the report or chapter object.
 ##' @return data.frame with the found references.
 ##' @export
-references <- function(x) UseMethod("references")
-
-##' @export
-references.report <- function(x) {
-    references(chapters(x))
+references <- function() {
+    if (in_chapter()) {
+        return(references_chapter())
+    } else if (in_report()) {
+        return(do.call("rbind", lapply(list.files("chapters"), function(chapter) {
+            wd <- setwd(paste0("chapters/", chapter))
+            ref <- references()
+            setwd(wd)
+            ref
+        })))
+    }
 }
 
-##' @export
-references.chapters <- function(x) {
-    do.call("rbind", lapply(x$section, function(y) {
-        wd <- setwd(chapter_path(y))
-        ref <- references(y)
-        setwd(wd)
-        ref
-    }))
-}
-
-##' @export
-references.chapter <- function(x) {
+references_chapter <- function() {
     pattern <- "[\\]label[{][^}]*[}]|[\\]ref[{][^}]*[}]"
-    files <- chapter_tex_files(x)
+    files <- chapter_tex_files()
 
     do.call("rbind", (lapply(files, function(filename) {
         tex <- readLines(filename)
@@ -72,7 +66,7 @@ references.chapter <- function(x) {
         }))
 
         if (length(m)) {
-            filename = file.path(chapter_path(x), basename(filename))
+            filename = paste0("chapters/", basename(getwd()), "/", filename)
             tex <- m
             cmd <- sub("[\\]([^{]+)[{][^}]*[}]", "\\1", tex)
             marker <- sub("[\\][^{]+[{]([^}]*)[}]", "\\1", tex)
@@ -96,12 +90,10 @@ references.chapter <- function(x) {
 
 ##' Get the chapter tex files
 ##'
-##' @param x the chapter object
 ##' @importFrom methods is
 ##' @keywords internal
-chapter_tex_files <- function(x, type = c("all", "text", "fig", "table")) {
+chapter_tex_files <- function(type = c("all", "text", "fig", "table")) {
     type = match.arg(type)
-    stopifnot(inherits(x, "chapter"))
 
     text_files <- NULL
     fig_files  <- NULL
@@ -110,9 +102,9 @@ chapter_tex_files <- function(x, type = c("all", "text", "fig", "table")) {
     if (type %in% c("all", "text"))
         text_files <- "text.tex"
     if (type %in% c("all", "fig"))
-        fig_files <- figure_files(x, "tex")
+        fig_files <- figure_files("tex")
     if (type %in% c("all", "table"))
-        tab_files <- table_files(x, "tex")
+        tab_files <- table_files("tex")
 
     c(text_files, fig_files, tab_files)
 }
