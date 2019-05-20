ecc187de44f6eb7305e596012d84cc209f872c37
Stefan Widgren <stefan.widgren@gmail.com>
2019-05-20 22:39:46 +0200

    Add initial work to convert docx tables to tex

---
+++ R/DrTeX.R
@@ -0,0 +1,518 @@
+##' Load all tables in a docx file
+##'
+##' @param filename name of the docx file.
+##' @importFrom utils unzip
+##' @importFrom xml2 xml_find_all
+##' @importFrom xml2 xml_text
+##' @importFrom xml2 read_xml
+##' @export
+##' @examples
+##' ## Load tables from a bundled docx file.
+##' tbl <- docx_tables(system.file("extdata/table.docx", package = "doctex"))
+##'
+##' ## View the layout of the tables
+##' tbl
+##'
+##' ## View the column widths
+##' sapply(tbl, col_widths)
+docx_tables <- function(filename)
+{
+    on.exit(unlink(file.path(tempdir(), "document.xml")), add = TRUE)
+
+    ## Unzip the content of the word file.
+    unzip(filename, "word/document.xml", junkpaths = TRUE, exdir = tempdir())
+
+    ## Parse the content of the word file
+    doc <- read_xml(file.path(tempdir(), "document.xml"))
+
+    ## Extract the tables
+    x <- xml_find_all(doc, xpath = "//w:tbl")
+    tbls <- lapply(seq_len(length(x)), function(i) {
+        docx_table(x[[i]])
+    })
+
+    tbls
+}
+
+##' Create a docx table object
+##'
+##' A \code{docx_table} consists of a caption, a label, table data and
+##' footnotes.
+##' @param xml the xml node for the docx table.
+##' @importFrom xml2 xml_find_first
+##' @importFrom xml2 xml_name
+##' @return a \code{docx_table} object.
+##' @export
+docx_table <- function(xml)
+{
+    stopifnot(identical(xml_name(xml), "tbl"))
+    p <- xml_find_first(xml, "preceding-sibling::w:p[1]")
+    structure(list(caption  = docx_caption(p),
+                   label    = docx_label(p),
+                   content  = xml,
+                   footnote = docx_footnote(NULL)),
+              class = "docx_table")
+}
+
+##' Create a docx caption object
+##'
+##' @param xml the xml node for the docx caption.
+##' @importFrom xml2 xml_name
+##' @return a \code{docx_caption} object.
+##' @export
+docx_caption <- function(xml)
+{
+    stopifnot(identical(xml_name(xml), "p"))
+    structure(list(content = xml), class = "docx_caption")
+}
+
+##' @importFrom xml2 xml_text
+##' @export
+format.docx_caption <- function(x, output = c("ascii", "tex"), prefix = "", ...)
+{
+    if (match.arg(output) == "ascii")
+        return(xml_text(x$content))
+
+    ## Determine the label for the caption.
+    pattern <- "^(Table|Figure)[[:space:]]*[[](tab|fig):[^]]+[]]:[[:space:]]*"
+    str <- sub(pattern, "", xml_text(x$content))
+
+    paste0("\\caption{", str, "}\n")
+}
+
+##' @export
+print.docx_caption <- function(x, ...)
+{
+    cat(format(x, ...), "\n", sep = "")
+    invisible(x)
+}
+
+##' Create a docx label object
+##'
+##' @param xml the xml node for the docx label.
+##' @importFrom xml2 xml_name
+##' @return a \code{docx_label} object.
+##' @export
+docx_label <- function(xml)
+{
+    stopifnot(identical(xml_name(xml), "p"))
+    structure(list(content = xml), class = "docx_label")
+}
+
+##' @importFrom xml2 xml_text
+##' @export
+format.docx_label <- function(x, output = c("ascii", "tex"), prefix = "", ...)
+{
+    if (match.arg(output) == "ascii")
+        return("")
+
+    pattern <- "^(Table|Figure)[[:space:]]*[[](tab|fig):[^]]+[]]:[[:space:]]*"
+    str <- xml_text(x$content)
+    lbl <- regmatches(str, regexpr(pattern, str))
+    lbl <- sub("^Table[[:space:]]*[[]", "", sub("[]]:[[:space:]]*$", "", lbl))
+
+    ## Check if the label should be prefixed e.g. 'tab:cattle' ->
+    ## 'tab:prefix:cattle'.
+    if (nchar(prefix))
+        lbl <- sub("^tab:", paste0("tab:", prefix, ":"), lbl)
+
+    paste0("\\label{", lbl, "}\n")
+}
+
+##' @export
+print.docx_label <- function(x, ...)
+{
+    cat(format(x, ...), "\n", sep = "")
+    invisible(x)
+}
+
+##' @export
+length.docx_label <- function(x)
+{
+    nchar(xml_text(x$content))
+}
+
+##' Create a docx footnote object
+##'
+##' @param xml the xml node for the docx footnotes.
+##' @importFrom xml2 xml_name
+##' @return a \code{docx_footnote} object.
+##' @export
+docx_footnote <- function(xml)
+{
+    ## stopifnot(identical(xml_name(xml), "p"))
+    structure(list(content = xml), class = "docx_footnote")
+}
+
+##' @export
+length.docx_footnote <- function(x)
+{
+    0L
+}
+
+##' Column widths
+##'
+##' Get the columns widths of a docx table.
+##' @param tbl table to get the column widths from.
+##' @return numeric vector with column widths.
+##' @importFrom xml2 xml_attr
+##' @export
+col_widths <- function(tbl)
+{
+    as.numeric(xml_attr(xml_children(xml_child(tbl$content, "w:tblGrid")), "w"))
+}
+
+##' @importFrom xml2 xml_child
+##' @importFrom xml2 xml_children
+##' @importFrom xml2 xml_length
+##' @importFrom xml2 xml_name
+##' @export
+dim.docx_table <- function(x)
+{
+    c(sum(xml_name(xml_children(x$content)) == "tr"),
+      xml_length(xml_child(x$content, "w:tblGrid")))
+}
+
+##' @importFrom xml2 xml_find_all
+##' @export
+dim.docx_w_tr <- function(x)
+{
+    c(1, length(xml_find_all(x$content, "w:tc")))
+}
+
+##' Dimensions of a cell.
+##'
+##' Note that if the cell is part of a vertically merged region, then
+##' the first dimension is \code{NA}.
+##' @param x the cell to get the dimension from
+##' @importFrom xml2 xml_attr
+##' @importFrom xml2 xml_find_first
+##' @export
+dim.docx_w_tc <- function(x)
+{
+    ## Check if the cell is part of a vertically merged region.
+    i <- xml_find_first(x$content, "w:tcPr/w:vMerge")
+    if (is.na(i)) {
+        i <- 1
+    } else {
+        ## The cell is part of a vertically merged region. Let the
+        ## number of rows be undefined.
+        i <- NA_integer_
+    }
+
+    ## Check if the cell is part of a horizontically merged region.
+    j <- as.numeric(xml_attr(xml_find_first(x$content, "w:tcPr/w:gridSpan"), "val"))
+    if (is.na(j))
+        j <- 1
+
+    c(i, j)
+}
+
+##' Determine if a cell is part of vertically merged region
+##'
+##' @param tbl the table.
+##' @param i the index to the current row.
+##' @param j the index to the current cell in the current row.
+##' @noRd
+is_vmerge_region <- function(tbl, i, j)
+{
+    is.na(nrow(tbl[i][j]))
+}
+
+##' Determine if a cell continues a vertically merged region.
+##'
+##' Find the cell below and make sure is starts at the same grid
+##' column and that the cells have identical col-span.
+##' @param tbl the table.
+##' @param i the index to the current row.
+##' @param j the index to the current cell in the current row.
+##' @noRd
+vmerge_continue <- function(tbl, i, j)
+{
+    ## Last row?
+    if (i >= nrow(tbl))
+        return(FALSE)
+
+    ## Current and next row.
+    row <- tbl[i]
+    next_row <- tbl[i+1]
+
+    ## Determine the grid column for where the current cell in the
+    ## current row starts.
+    grid_j <- 1 + sum(vapply(seq_len(j-1), function(jj) {
+        ncol(row[jj])
+    }, numeric(1)))
+
+    ## Try to locate a cell below.
+    ncol_below <- ncol(next_row)
+    grid_j_below <- 1
+    j_below <- 1
+    repeat {
+        if (j_below > ncol_below || grid_j_below > grid_j)
+            return(FALSE)
+        if (grid_j_below == grid_j)
+            break
+        grid_j_below <- grid_j_below + ncol(next_row[j_below])
+        j_below <- j_below + 1
+    }
+    cell_below <- next_row[j_below]
+
+    ## Check that the col-span of the current cell and the cell below
+    ## is identical.
+    if (!identical(ncol(row[j]), ncol(cell_below)))
+        return(FALSE)
+
+    ## Finally, check if the cell below continues a vertically merged
+    ## region.
+    merge <- xml_find_first(cell_below$content, "w:tcPr/w:vMerge")
+    if (is.na(merge))
+        return(FALSE)
+
+    ## If the attribute value is equal to 'restart' then start a new
+    ## vertically merged region in this table. If this attribute is
+    ## 'continue' or omitted, continue the vertically merged region.
+    val <- xml_attr(merge, "val")
+    if (is.na(val) || val == "continue")
+        return(TRUE)
+    FALSE
+}
+
+format_docx_table_as_ascii <- function(tbl, output, ...)
+{
+    ## Line for the top and bottom borders.
+    l <- paste0("+", paste0(rep("---", ncol(tbl)), collapse = "+"), "+")
+
+    lines <- paste0(format(tbl$caption, output), "\n")
+    for (i in seq_len(nrow(tbl))) {
+        if (i == 1)
+            lines <- c(lines, paste0(l, "\n"))
+
+        row <- tbl[i]
+        for (j in seq_len(ncol(row))) {
+            ## Start each row with a vertical border '|' and a '+'.
+            if (j == 1) {
+                vb <- "|"
+                hb <- "+"
+            }
+
+            ## Col-span of the current cell.
+            nc <- ncol(row[j])
+
+            ## Append a vertical border ('|') to the next cell on the
+            ## right.
+            vb <- paste0(vb, paste0(rep("   ", nc), collapse = " "), "|")
+
+            ## Append a horizontal border ('-') to the next cell below
+            ## if the cell is not part a vertically merged region that
+            ## continues.
+            if (is_vmerge_region(tbl, i, j) && vmerge_continue(tbl, i, j)) {
+                b <- "   "
+            } else {
+                b <- "---"
+            }
+            hb <- paste0(hb, paste0(rep(b, nc), collapse = "+"), "+")
+        }
+
+        ## Last row?
+        if (i == nrow(tbl))
+            hb <- l
+
+        lines <- c(lines, paste0(vb, "\n"))
+        lines <- c(lines, paste0(hb, "\n"))
+    }
+
+    lines
+}
+
+top_border <- function(tbl, lines, indentation)
+{
+    ## Check if every cell in the firts row contains a top border.
+    if (nrow(tbl) > 0) {
+        xpath <- "w:tc/w:tcPr/w:tcBorders/w:top"
+        b <- xml_find_all(tbl[1]$content, xpath)
+        if (length(b) == ncol(tbl))
+            lines <- c(lines, paste0(indentation,"\\toprule\n"))
+    }
+
+    lines
+}
+
+bottom_border <- function(tbl, i, lines, indentation)
+{
+    ## Check if every cell in the row contains a bottom border.
+    xpath <- "w:tc/w:tcPr/w:tcBorders/w:bottom"
+    b <- xml_find_all(tbl[i]$content, xpath)
+    if (length(b) == ncol(tbl)) {
+        border <- ifelse(i < nrow(tbl), "\\midrule", "\\bottomrule")
+        lines <- c(lines, paste0(indentation, border, "\n"))
+    }
+
+    lines
+}
+
+has_footnotes <- function(tbl)
+{
+    FALSE
+}
+
+format_docx_table_as_tex <- function(tbl, output, indentation = "", standalone = FALSE, threeparttable = FALSE, ...)
+{
+    lines <- character(0)
+
+    if (isTRUE(standalone)) {
+        lines <- c(lines, paste0(indentation, "\\documentclass{article}\n"))
+        lines <- c(lines, paste0(indentation, "\\usepackage{booktabs}\n"))
+        lines <- c(lines, paste0(indentation, "\\usepackage[margin=1in]{geometry}\n"))
+        if (length(tbl$footnote) || isTRUE(threeparttable))
+            lines <- c(lines, paste0(indentation, "\\usepackage{threeparttable}\n"))
+        lines <- c(lines, paste0(indentation, "\\begin{document}\n"))
+    }
+
+    lines <- c(lines, paste0(indentation, "\\begin{table}\n"))
+
+    if (length(tbl$footnote) || isTRUE(threeparttable)) {
+        indentation <- paste0("  ", indentation)
+        lines <- c(lines, paste0(indentation, "\\begin{threeparttable}\n"))
+    }
+
+    indentation <- paste0("  ", indentation)
+    lines <- c(lines, paste0(indentation, format(tbl$caption, output, ...)))
+
+    lines <- c(lines, paste0(indentation,
+                             "\\begin{tabular}{",
+                             paste0(rep("l", dim(tbl)[2]), collapse = ""),
+                             "}\n"))
+
+    indentation <- paste0("  ", indentation)
+    lines <- top_border(tbl, lines, indentation)
+
+    for (i in seq_len(nrow(tbl))) {
+        row <- tbl[i]
+        line <- indentation
+
+        for (j in seq_len(ncol(row))) {
+            cell <- row[j]
+            value <- format(cell, output, ...)
+
+            if (ncol(cell) > 1) {
+                line <- paste0(line, "\\multicolumn{", ncol(cell), "}{l}{", value, "}")
+            } else {
+                line <- paste0(line, value)
+            }
+
+            if (j == ncol(row)) {
+                line <- paste0(line, " \\\\\n")
+            } else {
+                line <- paste0(line, " & ")
+            }
+        }
+
+        lines <- c(lines, line)
+        lines <- bottom_border(tbl, i, lines, indentation)
+    }
+
+    indentation <- substr(indentation, 3, nchar(indentation))
+    lines <- c(lines, paste0(indentation, "\\end{tabular}\n"))
+
+    if (length(tbl$footnote) || isTRUE(threeparttable)) {
+        if (length(tbl$footnote)){
+            lines <- c(lines, paste0(indentation, "\\begin{tablenotes}\n"))
+            ## FIXME: Write items.
+            ## \item\textsuperscript{A} 2 forest reindeer 1 blesbok.
+            lines <- c(lines, paste0(indentation, "\\end{tablenotes}\n"))
+        }
+
+        indentation <- substr(indentation, 3, nchar(indentation))
+        lines <- c(lines, paste0(indentation, "\\end{threeparttable}\n"))
+    }
+
+    if (length(tbl$label))
+        lines <- c(lines, paste0(indentation, format(tbl$label, output, ...)))
+
+    indentation <- substr(indentation, 3, nchar(indentation))
+    lines <- c(lines, paste0(indentation, "\\end{table}\n"))
+
+    if (isTRUE(standalone)) {
+        indentation <- substr(indentation, 3, nchar(indentation))
+        lines <- c(lines, paste0(indentation, "\\end{document}\n"))
+    }
+
+    lines
+}
+
+##' @export
+format.docx_table <- function(x, output = c("ascii", "tex"), ...)
+{
+    f <- switch(match.arg(output),
+                ascii = format_docx_table_as_ascii,
+                tex   = format_docx_table_as_tex)
+
+    f(tbl = x, output = output, ...)
+}
+
+##' @export
+print.docx_table <- function(x, ...)
+{
+    cat(format(x, ...), "\n", sep = "")
+    invisible(x)
+}
+
+##' @export
+`[.docx_table` <- function(x, i, j, drop = TRUE)
+{
+    r <- xml_find_all(x$content, "w:tr")
+
+    rows <- lapply(seq_len(length(r)), function(r_i) {
+        structure(list(content = r[[r_i]]), class = "docx_w_tr")
+    })[i]
+
+    if (isTRUE(drop) && length(i) == 1)
+        rows <- rows[[1]]
+
+    rows
+}
+
+##' @export
+`[.docx_w_tr` <- function(x, i, j, drop = TRUE)
+{
+    tc <- xml_find_all(x$content, "w:tc")
+
+    cols <- lapply(seq_len(length(tc)), function(tc_i) {
+        structure(list(content = tc[[tc_i]]), class = "docx_w_tc")
+    })[i]
+
+    if (isTRUE(drop) && length(i) == 1)
+        cols <- cols[[1]]
+
+    cols
+}
+
+##' @export
+format.docx_w_tr <- function(x, ...)
+{
+    l1 <- paste0("+", paste0(rep("---", ncol(x)), collapse = "+"), "+\n")
+    l2 <- paste0("|", paste0(rep("   ", ncol(x)), collapse = "|"), "|\n")
+
+    c(l1, l2, l1)
+}
+
+##' @export
+print.docx_w_tr <- function(x, ...)
+{
+    cat(format(x, ...), "\n", sep = "")
+    invisible(x)
+}
+
+##' @importFrom xml2 xml_text
+##' @export
+format.docx_w_tc <- function(x, ...)
+{
+    xml_text(x$content)
+}
+
+##' @export
+print.docx_w_tc <- function(x, ...)
+{
+    cat(format(x, ...), "\n", sep = "")
+    invisible(x)
+}
