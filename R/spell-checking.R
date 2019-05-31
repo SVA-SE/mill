794862bcf8f51defc1f7268a9b2ede8bfd061861
Stefan Widgren <stefan.widgren@sva.se>
2019-05-31 16:15:02 +0200

    Tweak spell checking

--- R/spell-checking.R
+++ R/spell-checking.R
@@ -14,16 +14,24 @@ spell_checking <- function()
 
         lines <- readLines("text.tex")
         bad_words <- hunspell(lines, format = "latex", dict = "en_GB", ignore = ignore)
+        bad_words <- unlist(bad_words)
 
         cat("\n**********\n*\n*", basename(getwd()), "\n*\n**********\n\n")
-        cat(sort(unique(unlist(bad_words))), sep = "\n")
+        cat(sort(unique(bad_words)), sep = "\n")
+        cat("\n")
     } else if (in_report()) {
-        lapply(list.files("chapters"), function(chapter) {
+        bad_words <- lapply(list.files("chapters"), function(chapter) {
             wd <- setwd(paste0("chapters/", chapter))
-            spell_checking()
+            result <- spell_checking()
             setwd(wd)
+            result
         })
+
+        bad_words <- as.character(unlist(bad_words))
+        bad_words <- as.data.frame(table(bad_words))
+        bad_words <- bad_words[order(bad_words$Freq, decreasing = TRUE), , drop = FALSE]
+        rownames(bad_words) <- NULL
     }
 
-    invisible()
+    invisible(bad_words)
 }
