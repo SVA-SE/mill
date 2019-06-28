5a6153b1b3ef30ee0ca5200278d333e28979a7b5
Stefan Widgren <stefan.widgren@sva.se>
2019-06-28 14:09:21 +0200

    Remove non-working example

--- R/mill.R
+++ R/mill.R
@@ -45,22 +45,12 @@ authors.authors <- function(x) {
 ##' solved with a hack that might have a better cross platform
 ##' solution.
 ##'
-##' @title formatted authors
 ##' @param x A report object
 ##' @param format The format of the author list you want
 ##' @return A unique sorted list of authors
 ##' @author Thomas Rosendal
 ##' @importFrom utils tail
 ##' @export
-##' @examples
-##' \dontrun{
-##' ## Update the authors list for front matter
-##' writeLines(paste(formatted_authors(load_report()),
-##'                  collapse = ",\n"),
-##'            "assets/front-matter/authors.tex")
-##' ## Or get the emails in a send list format
-##' paste(formatted_authors(load_report(), format = "email"), collapse = ";")
-##' }
 formatted_authors <- function(x, format = c("name", "email")) {
     format <- match.arg(format)
     auths <- authors(x)
