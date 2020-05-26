6a9d1e92e317b57b4284b390a5bee7ee5b781544
Stefan Widgren <stefan.widgren@sva.se>
2020-05-26 18:12:28 +0200

    Fix path

--- R/check.R
+++ R/check.R
@@ -346,7 +346,7 @@ check_figure_reference_files <- function() {
         if (nrow(ref)) {
             id <- sapply(strsplit(ref$marker, ":"), "[", 3)
             filename <- paste0(chapter, "/fig_",
-                               normalize_title(basename(chapter)),
+                               normalize_title(basename(getwd())),
                                "_", id, ".tex")
             ref_fig_files <- filename
         } else {
