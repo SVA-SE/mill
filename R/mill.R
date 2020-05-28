7bd6d7169db33fff733afbfe1a2d5c64320a97b5
Stefan Widgren <stefan.widgren@sva.se>
2020-05-28 12:41:56 +0200

    Add 'NUTS_20M' and 'kommuner' datasets from svamap

--- R/mill.R
+++ R/mill.R
@@ -232,3 +232,32 @@ chapter_path <- function(x) {
 ##' @keywords datasets
 ##' @import sp
 NULL
+
+##' Administrative (NUTS) units in Sweden (1 : 20 Million)
+##'
+##' The dataset contains administrative units (NUTS) in Sweden in
+##' European terminology in scale 1 : 20 Million.
+##'
+##' Reprojected to WGS84
+##'
+##' @name NUTS_20M
+##' @docType data
+##' @source
+##' \url{http://epp.eurostat.ec.europa.eu/portal/page/portal/gisco_Geographical_information_maps/popups/references/administrative_units_statistical_units_1}
+##' @keywords datasets
+##' @import sp
+NULL
+
+##' Administrative units in Sweden (Kommun)
+##'
+##' The dataset contains boundaries of kommun in Sweden
+##'
+##' Reprojected to WGS84
+##'
+##' @name kommuner
+##' @docType data
+##' @source
+##' \url{http://www.scb.se/Grupp/Produkter_Tjanster/Verktyg/_Dokument/Shape-svenska.zip}
+##' @import sp
+##' @keywords datasets
+NULL
