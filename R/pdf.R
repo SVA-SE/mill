780a9b171c494cf4d7034e7d5c4ad644fe9b7edf
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2020-05-30 13:12:08 +0200

    Remove copy of missing-photo.png (file doesn't exist anymore)

--- R/pdf.R
+++ R/pdf.R
@@ -152,10 +152,6 @@ to_pdf_chapter <- function(build = TRUE, type = c("print", "web")) {
                  tex[seq(placement, length(tex))])
         writeLines(tex, filename)
 
-        ## copy in the missing-image file
-        on.exit(file.remove("missing-photo.png"))
-        file.copy("../../assets/missing-photo.png", ".")
-
         ## Build the filename pdf file.
         luatex(filename)
     } else {
