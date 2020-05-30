9b865628d20a9cf2e578348ae79cedbe7f8b76b3
Thomas Rosendal <thomas.rosendal@sva.se>
2020-05-30 21:18:10 +0200

    create patch failing to create patch file for figures

---
+++ tests/patch_diff.R
@@ -0,0 +1,34 @@
+setwd("chapters/test1")
+mill::apply_patch()
+
+## Ensure that the two new files were created
+ob <- list.files()
+ex <- c("fig_test1_timeseries.tex",
+        "fig_timeseries.tex",
+        "text.tex",
+        "typeset.tex")
+stopifnot(identical(ob, ex))
+
+## Because no changes were made there should be no patches created:
+mill::create_patch()
+ob <- list.files()
+stopifnot(identical(ob, ex))
+
+## If we make a change to typeset and fig_test1_timeseries.tex then we
+## expect patches to be written.
+writeLines(c(readLines("fig_test1_timeseries.tex"),
+             "bar"), con = "fig_test1_timeseries.tex")
+
+writeLines(c(readLines("typeset.tex"),
+             "bar"), con = "typeset.tex")
+
+## Now create the patches.
+mill::create_patch()
+ob <- list.files()
+ex <- c("fig_test1_timeseries.tex",
+        "fig_timeseries.patch",
+        "fig_timeseries.tex",
+        "text.tex",
+        "typeset.patch",
+        "typeset.tex")
+stopifnot(identical(ob, ex))
