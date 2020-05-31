45ef010146259b2aef8816aa5070ea389f04b1be
Stefan Widgren <stefan.widgren@sva.se>
2020-05-31 14:37:17 +0200

    spell check: make it possible to harvest words interactively

--- R/spell-checking.R
+++ R/spell-checking.R
@@ -1,10 +1,21 @@
 ##' Spell checking of chapters
 ##'
+##' @param harvest \code{TRUE} if you want to harvest the words and
+##'     add them to the report-level [r] or chapter-level [c]
+##'     WORDLIST. Harvesting can only be used in an interactive
+##'     session.
+##' @return a character vector with the bad words invisible.
 ##' @importFrom hunspell hunspell
 ##' @export
-spell_checking <- function() {
+spell_checking <- function(harvest = FALSE) {
     if (in_chapter()) {
-        ## Make sure to add a WORDLIST file in each chapter.
+        ## Make sure to add a WORDLIST file in the report root and in
+        ## each chapter.
+        if (!file.exists("../../WORDLIST")) {
+            file.create("../../WORDLIST")
+            git2r::add(repository(), "WORDLIST")
+        }
+
         if (!file.exists("WORDLIST")) {
             file.create("WORDLIST")
             git2r::add(repository(), "WORDLIST")
@@ -25,6 +36,27 @@ spell_checking <- function() {
         cat("\n**********\n*\n*", basename(getwd()), "\n*\n**********\n\n")
         cat(sort(unique(bad_words)), sep = "\n")
         cat("\n")
+
+        if (interactive() && isTRUE(harvest)) {
+            for (i in seq_len(length(bad_words))) {
+                m <- sprintf("Add '%s' [(r)report|(c)hapter|(s)kip|(q)uit]? ",
+                             bad_words[i])
+                x <- readline(m)
+                if (tolower(substr(x, 1, 1)) == "r") {
+                    w <- sort(c(bad_words[i], readLines("../../WORDLIST")))
+                    writeLines(w, "../../WORDLIST")
+                } else if (tolower(substr(x, 1, 1)) == "c") {
+                    w <- sort(c(bad_words[i], readLines("WORDLIST")))
+                    writeLines(w, "WORDLIST")
+                } else if (tolower(substr(x, 1, 1)) == "s") {
+                    next
+                } else if (tolower(substr(x, 1, 1)) == "q") {
+                    break
+                } else {
+                    stop(sprintf("Unknown option: '%s'", x))
+                }
+            }
+        }
     } else if (in_report()) {
         ## Make sure to add a WORDLIST file to the project.
         if (!file.exists("WORDLIST")) {
@@ -34,7 +66,7 @@ spell_checking <- function() {
 
         bad_words <- lapply(list.files("chapters"), function(chapter) {
             wd <- setwd(paste0("chapters/", chapter))
-            result <- spell_checking()
+            result <- spell_checking(harvest = harvest)
             setwd(wd)
             result
         })
@@ -44,6 +76,9 @@ spell_checking <- function() {
         bad_words <- bad_words[order(bad_words$Freq, decreasing = TRUE), ,
                                drop = FALSE]
         rownames(bad_words) <- NULL
+    } else{
+        stop("You must run spell-checking from ",
+             "the report root or in a chapter.")
     }
 
     invisible(bad_words)
