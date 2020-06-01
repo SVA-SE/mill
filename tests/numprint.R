115cc6dca15051d336c2dc099315c4cc3bcc7d37
Stefan Widgren <stefan.widgren@sva.se>
2020-06-01 13:37:26 +0200

    tests: test numprint in a figure tex-file

--- tests/numprint.R
+++ tests/numprint.R
@@ -26,3 +26,34 @@ stopifnot(identical(ob, ex))
 ob <- mill:::style_numprint("dnr 6.2.18-14271/2018")
 ex <- "dnr 6.2.18-14271/2018"
 stopifnot(identical(ob, ex))
+
+## Test numprint in a figure tex-file.
+tex <- c(
+"\\begin{figure}[H]",
+paste0("  \\includegraphics[width=\\textwidth]",
+       "{fig_verotoxinogenic-escherichia-coli_timeseries}"),
+"  \\caption{Incidence (per 100000 inhabitants) of",
+"    notified human STEC cases in Sweden, 1997--2019. Prior to 2005, only",
+"    O157 was required to be reported. In 2005, all serogroups of STEC",
+"    including PCR findings became subject for notification. In 2005, 2016",
+"    and 2018, the number of cases increased due to one or more domestic",
+"    outbreaks.",
+"    \\label{fig:verotoxinogenic-escherichia-coli:timeseries}",
+"  }",
+"\\end{figure}")
+
+tex_exp <- c(
+"\\begin{figure}[H]",
+paste0("  \\includegraphics[width=\\textwidth]",
+       "{fig_verotoxinogenic-escherichia-coli_timeseries}"),
+"  \\caption{Incidence (per \\numprint{100000} inhabitants) of",
+"    notified human STEC cases in Sweden, 1997--2019. Prior to 2005, only",
+"    O157 was required to be reported. In 2005, all serogroups of STEC",
+"    including PCR findings became subject for notification. In 2005, 2016",
+"    and 2018, the number of cases increased due to one or more domestic",
+"    outbreaks.",
+"    \\label{fig:verotoxinogenic-escherichia-coli:timeseries}",
+"  }",
+"\\end{figure}")
+
+stopifnot(identical(mill:::style_numprint(tex), tex_exp))
