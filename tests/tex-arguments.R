5d27dd26df1713c750e98a107a924968bfe88151
Stefan Widgren <stefan.widgren@sva.se>
2020-06-01 23:57:04 +0200

    tests: add regression test for 'tex_arguments'

---
+++ tests/tex-arguments.R
@@ -0,0 +1,24 @@
+library(mill)
+
+tex <- c(
+    paste0("\\hypertarget{sec:animal-databases:animal-registers-",
+           "and-data-sources-used-in-surveillance}{%"),
+    "\\chapter*{Animal registers and data sources used in",
+    paste0("surveillance}\\label{sec:animal-databases:animal-",
+           "registers-and-data-sources-used-in-surveillance}}"),
+    "",
+    "\\hypertarget{sec:animal-databases:the-central-register-of-holdings}{%",
+    "\\section*{The Central Register of",
+    "Holdings}\\label{sec:animal-databases:the-central-register-of-holdings}}",
+    "",
+    "The Swedish Board of Agriculture is responsible for maintaining the")
+
+tex <- mill:::tex_2_one_line(tex)
+
+stopifnot(identical(mill:::tex_arguments(substr(tex, 13, nchar(tex))),
+                    list(paste0("sec:animal-databases:animal-registers-",
+                                "and-data-sources-used-in-surveillance"),
+                         paste0("%\n\\chapter*{Animal registers and data ",
+                                "sources used in\nsurveillance}\\label{",
+                                "sec:animal-databases:animal-registers-and-",
+                                "data-sources-used-in-surveillance}"))))
